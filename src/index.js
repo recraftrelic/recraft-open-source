a5ac697c863ef0e83f0594efb8bba20837cf78bf
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-02-27 19:23:06 +0530

    ui-design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '../node_modules/bulma/css/bulma.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
