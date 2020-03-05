04b06f90ce5f2b93a5fd10c2c3d202d5b3f8a4bd
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-03-05 11:34:56 +0530

    small-changes

--- src/components/ProjectItems.js
+++ src/components/ProjectItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/App.css';
 
 export default class ProjectItems extends React.Component
 {
@@ -7,12 +8,12 @@ export default class ProjectItems extends React.Component
           <>
             <div className={this.props.left}>
               <figure className="image is-256x256">
-                <img src={require(`../images/${this.props.items.image}`)} alt="chat-app" />
+                <a target="_blank" href={this.props.items.link}><img src={require(`../images/${this.props.items.image}`)} alt="chat-app" /></a>
               </figure>
             </div>
             <div className={this.props.right}>
               <h1 className="title has-text-left extra-space">
-                <a className="is-text has-text-black" href={this.props.items.link}>{this.props.items.heading}</a>
+                <a target="_blank" className="is-text has-text-black" href={this.props.items.link}>{this.props.items.heading}</a>
               </h1>
               <p className="bd-notification is-primary has-text-left is-size-5">{this.props.items.mainDesc}</p>
               <p className="bd-notification is-primary has-text-left is-size-5">{this.props.items.secondDesc}</p>
@@ -20,9 +21,9 @@ export default class ProjectItems extends React.Component
                 {
                     this.props.items.technologies.map((data,index) => {
                         return <>
-                          <div className="column is-1">
+                          <div className="techno">
                             <figure className="image is-32x32">
-                              <a href={data.link}><img src={require(`../images/${data.icon}`)} alt="technologies"/></a>
+                              <a target="_blank" href={data.link}><img src={require(`../images/${data.icon}`)} alt="technologies"/></a>
                             </figure>
                           </div>
                         </>
@@ -33,7 +34,7 @@ export default class ProjectItems extends React.Component
                     this.props.items.contribution.map((data,index) => {
                       return <>
                         <p className="bd-notification is-primary has-text-left">{data.desc}<br></br>
-                          <a className="is-text" href={data.links}>{data.links}</a>
+                          <a target="_blank" className="is-text" href={data.links}>{data.links}</a>
                         </p>
                       </>
                     })
