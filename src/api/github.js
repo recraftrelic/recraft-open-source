6b00f8b686d8bbf11496f979658faaa1b51a53a6
puranchand <pchand757@gmail.com>
2020-03-19 19:14:42 +0530

    display github commit list

--- src/api/github.js
+++ src/api/github.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-// export const getNativeChatAppCommit = () => {
-//    return  axios.get(`https://api.github.com/repos/recraftrelic/react-native-chat-app-boilerplate/commits`)
-// }
+export const getGithubData = (url) => {
+   return axios.get(`https://api.github.com/repos/${url}`)
+}
--- src/components/GithubProjects.js
+++ src/components/GithubProjects.js
@@ -1,79 +1,78 @@
 import React from 'react';
-import axios from "axios";
+import { getGithubData } from '../api/github';
+import GithubCommitsItem from './GithubCommitsItem';
 
 export default class GithubProjects extends React.Component {
 
   state = {
     repo: "",
-    contributor: {
-      avatar: "",
-      name: "",
-      commitLink: ""
-    }
+    repoLink: "",
+    commitList: []
   }
 
   componentDidMount() {
-      axios.get(`https://api.github.com/repos/recraftrelic/react-native-chat-app-boilerplate`)
-      .then(result => {
-        if(result.data){
-          this.setState({
-            repo: result.data.name
-          })
-        }
-      })  
-      .catch(error =>
-        console.log(error)
-      );
+    const repoUrl = `recraftrelic/react-native-chat-app-boilerplate`;
+    const commitsUrl = `${repoUrl}/commits`;
 
-      axios.get(`https://api.github.com/repos/recraftrelic/react-native-chat-app-boilerplate/commits`)
+    getGithubData(repoUrl)
+    .then(result => {
+      if (result.data) {
+        this.setState({
+          repo: result.data.name,
+          repoLink: result.data.html_url,
+        })
+      }
+    })
+    .catch(error =>
+      console.log(error)
+    );
+
+    getGithubData(commitsUrl)
       .then(result => {
-        let data = result.data[0]
-        if(data){
-          
+        if (result.data) {
           this.setState({
-            contributor: {
-              avatar: data.author.avatar_url,
-              name: data.commit.author.name,
-              commitLink: data.html_url,
-            }
+            commitList: result.data
           })
         }
       })
       .catch(error =>
-       console.log(error)
+        console.log(error)
       );
   }
 
-    render () {
-      let {repo} = this.state
-      let {avatar, name, commitLink} = this.state.contributor
-        return (
-          <section className="section">
-            <div className="container">
-              <div className="columns is-mobile is-centered">
-                <div className="column is-8">
-                  <div className={"column is-8 is-pulled-left"}>
-                    <h1 className="title has-text-left extra-space">
-                      <a target="_blank" className="is-text has-text-black" href={"/"}>{repo}</a>
-                    </h1>
-                    <div className="columns is-gap extra-space">
-                      <div className="techno">
-                        <figure className="image is-64x64">
-                          <a target="_blank" href={"/"}>
-                            <img src={avatar} alt="technologies" />
-                          </a>
-                        </figure>
+  render() {
+    let { repo, repoLink, commitList } = this.state
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns is-mobile is-centered">
+            <div className="column is-8">
+              <div className={"column is-12 is-pulled-left has-text-left"}>
+                <h1 className="title has-text-left extra-space">
+                  <a target="_blank" className="is-text has-text-black" href={"/"}>{"Github Works"}</a>
+                </h1>
+                {commitList.length && commitList.map((commit, index) => {
+                  if(index < 10){
+                    return (
+                      <div key={index}>
+                        <GithubCommitsItem
+                          repo={repo}
+                          repoLink={repoLink}
+                          name={commit.commit.author.name}
+                          avatar={commit.author.avatar_url}
+                          commitId={commit.sha}
+                          commitLink={commit.html_url}
+                          commitUserProfile={commit.author.html_url}
+                        />
                       </div>
-                    </div>
-                    <p className="bd-notification is-primary has-text-left is-size-5">{name}</p>
-                    <p className="bd-notification is-primary has-text-left">{"Commit :- "}<br></br>
-                      <a target="_blank" className="is-text" href={commitLink}>{commitLink}</a>
-                    </p>
-                  </div>
-                </div>
+                    )
+                  }
+                })}
               </div>
             </div>
-          </section>
-        );
-    }
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
